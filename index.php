2f6cc1b5d5415f78d58f6baffd1059d481a19a9b
hondatechkassim <111937306+hondatechkassim@users.noreply.github.com>
2022-08-31 14:56:12 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>sample</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+
+<p>homw is the best</p>
+
+<?php
+include 'db/conf.php';
+
+
+
+
+
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
+</body>
+</html>
+
